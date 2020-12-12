7c9c918019fc863c8ca724387af786a32e99aab8
Salman <salman@gmail.com>
2020-12-12 14:09:53 +0500

    php all data

---
+++ Masharib.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$salary = 500.55;
+$name = "Masharib<br>";
+echo"My name is $name";
+echo "My Salary is Rs.$salary";
+?>
+
+</body>
+</html>
+
+
+
+
